b85d235ecb053d6cc64a82a140c25a832d07bf11
datajake1999 <datajake@gmail.com>
2019-08-10 02:31:54 -0700

    Updated compile script to work with Visual Studio 2005 and build 64-bit dll.

--- compile.bat
+++ compile.bat
@@ -1,3 +1,10 @@
-cl -c /I ".\VST3 SDK" /I ".\include" "VST3 SDK\public.sdk\source\vst2.x\*.cpp" "src\*.cpp"
+@ECHO OFF
+call "c:\program files\microsoft visual studio 8\vc\bin\vcvars32.bat"
+call "C:\program files\Microsoft Platform SDK\SetEnv.Cmd" /SRV32 /RETAIL
+cl -c /I ".\include" /I ".\VST3_SDK" src\*.cpp VST3_SDK\public.sdk\source\vst2.x\*.cpp
 link *.obj bufferoverflowU.lib kernel32.lib user32.lib /dll /def:".\src\vstplug.def" /OUT:EAXReverb.dll
+del *.obj
+call "C:\program files\Microsoft Platform SDK\SetEnv.Cmd" /X64 /RETAIL
+cl -c /I ".\include" /I ".\VST3_SDK" src\*.cpp VST3_SDK\public.sdk\source\vst2.x\*.cpp
+link *.obj bufferoverflowU.lib kernel32.lib user32.lib /dll /def:".\src\vstplug.def" /OUT:EAXReverb_x64.dll
 del *.obj
