1d2041eb0855a5e6f551238209e97eabf59d8796
datajake1999 <datajake@gmail.com>
2019-08-21 03:47:06 -0700

    Added command to create zip file of compiled versions.

--- compile_msvc.bat
+++ compile_msvc.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-set path="C:\Program Files\Inno Setup 5"
+set path="C:\Program Files\7-Zip";"C:\Program Files\Inno Setup 5"
 call "c:\program files\microsoft visual studio 8\vc\bin\vcvars32.bat"
 call "C:\program files\Microsoft Platform SDK\SetEnv.Cmd" /SRV32 /RETAIL
 cl -c /I ".\include" /I ".\VST3_SDK" src\*.cpp VST3_SDK\public.sdk\source\vst2.x\*.cpp
@@ -10,3 +10,4 @@ cl -c /I ".\include" /I ".\VST3_SDK" src\*.cpp VST3_SDK\public.sdk\source\vst2.x
 link *.obj bufferoverflowU.lib kernel32.lib user32.lib /dll /def:".\src\vstplug.def" /OUT:EAXReverb_x64.dll
 del *.obj
 iscc "install.iss"
+7z a -tzip output\EAXReverb_VST.zip EAXReverb.dll EAXReverb_x64.dll
