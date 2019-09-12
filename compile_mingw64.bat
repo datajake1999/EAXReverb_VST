1d39bc5e99e77c4fd63b0bd0c063854f0015b6b6
datajake1999 <datajake@gmail.com>
2019-09-12 14:40:36 -0700

    Added resource script with version information.

--- compile_mingw64.bat
+++ compile_mingw64.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
 g++ -fPIC -c -IVST3_SDK -D__cdecl="" src\*.cpp VST3_SDK\public.sdk\source\vst2.x\*.cpp
+windres res\EAXReverb.rc resource.o
 g++ -s -shared -static -o EAXReverb_x64.dll *.o
 del *.o
--- compile_msvc.bat
+++ compile_msvc.bat
@@ -3,11 +3,13 @@ set path="C:\Program Files\7-Zip";"C:\Program Files\Inno Setup 5"
 call "c:\program files\microsoft visual studio 8\vc\bin\vcvars32.bat"
 call "C:\program files\Microsoft Platform SDK\SetEnv.Cmd" /SRV32 /RETAIL
 cl -c /I ".\include" /I ".\VST3_SDK" src\*.cpp VST3_SDK\public.sdk\source\vst2.x\*.cpp
-link *.obj bufferoverflowU.lib kernel32.lib user32.lib /dll /def:".\src\vstplug.def" /OUT:EAXReverb.dll
+rc res\EAXReverb.rc
+link *.obj res\EAXReverb.res bufferoverflowU.lib kernel32.lib user32.lib /dll /def:".\src\vstplug.def" /OUT:EAXReverb.dll
 del *.obj
 call "C:\program files\Microsoft Platform SDK\SetEnv.Cmd" /X64 /RETAIL
 cl -c /I ".\include" /I ".\VST3_SDK" src\*.cpp VST3_SDK\public.sdk\source\vst2.x\*.cpp
-link *.obj bufferoverflowU.lib kernel32.lib user32.lib /dll /def:".\src\vstplug.def" /OUT:EAXReverb_x64.dll
+link *.obj res\EAXReverb.res bufferoverflowU.lib kernel32.lib user32.lib /dll /def:".\src\vstplug.def" /OUT:EAXReverb_x64.dll
 del *.obj
+del res\EAXReverb.res
 iscc "install.iss"
 7z a -tzip output\EAXReverb_VST.zip EAXReverb.dll EAXReverb_x64.dll
